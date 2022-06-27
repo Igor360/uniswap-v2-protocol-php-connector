a47ed090435cfbd9621ba0e202c0b615701fb0c5
Igor360 <igor.dawidencko@gmail.com>
2022-06-27 13:12:23 +0300

    Updated config file

--- config/config.php
+++ config/config.php
@@ -1,5 +1,10 @@
 <?php
 return [
+    "routerABI" => \Igor360\UniswapV2Connector\Configs\ConfigFacade::loadRouterABI(),
+    "factoryABI" => \Igor360\UniswapV2Connector\Configs\ConfigFacade::loadFactoryABI(),
+    "pairABI" => \Igor360\UniswapV2Connector\Configs\ConfigFacade::loadPairABI(),
+    "erc20ABI" => \Igor360\UniswapV2Connector\Configs\ConfigFacade::loadERC20ABI(),
+    "erc721ABI" => \Igor360\UniswapV2Connector\Configs\ConfigFacade::loadERC721ABI(),
     "eth" => [
         "host" => "",
         "port" => "",
